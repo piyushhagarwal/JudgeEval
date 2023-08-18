5ebd81e7cf7932c3f296d5c1af3ad8828543e5f8
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-08-18 10:33:11 +0530

    Duplicate entries , and added comments for futher tasks

--- controllers/scores.ts
+++ controllers/scores.ts
@@ -1,11 +1,43 @@
 import { Request, Response } from "express";
 import queryDatabase from "../database/connection";
+//if judge_id,parameter_id and team_id is not present
 
 export const score = async (req: Request, res: Response) => {
   try {
     const { team_id, parameter_id, score_value } = req.body;
     const judge_id = req.headers.judge_id;
 
+    //To prevent adding scores in the database which have judge_id = NULL
+    if (!judge_id) {
+      return res.status(500).json({
+        message: "Judge id not found, Check auth token.",
+        success: false,
+      });
+    }
+
+    //To prevent adding scores in the database whose judge_id is not present
+    //To prevent adding scores in the database whose parameter_id is not present
+    //To prevent adding scores in the database whose team_id is not present
+
+    //To prevent adding duplicate scores in the database
+    const alreadyExistQuery = `
+      SELECT * FROM scores 
+      WHERE judge_id = $1 AND team_id = $2 AND parameter_id = $3;  
+    `;
+
+    const duplicateScore = await queryDatabase(alreadyExistQuery, [
+      judge_id,
+      team_id,
+      parameter_id,
+    ]);
+
+    if (duplicateScore.length !== 0 || !duplicateScore) {
+      return res
+        .status(500)
+        .json({ message: "Score already exists", success: false });
+    }
+
+    //Main query to add score
     const query = `
         INSERT INTO scores ( judge_id, team_id, parameter_id, score_value)
         VALUES ($1, $2, $3, $4)
@@ -18,6 +50,10 @@ export const score = async (req: Request, res: Response) => {
 
     res.status(201).json({ newScore: newScore[0], success: true });
   } catch (error) {
-    res.status(500).json({ message: "Error adding score:", success: false });
+    console.log(error);
+    res.status(500).json({
+      message: "Error adding score(Internal Server Error)",
+      success: false,
+    });
   }
 };
