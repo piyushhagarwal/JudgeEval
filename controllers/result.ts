e2b23b380dff33cdbc332324678cc64ebf0f0abf
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-08-22 15:32:03 +0530

    parameter wise result done

--- controllers/result.ts
+++ controllers/result.ts
@@ -46,6 +46,7 @@ export const getParameterWiseScores = async (req: Request, res: Response) => {
         SELECT
         t.team_id,
         t.team_name,
+        p.parameter_name,
         AVG(s.score_value) AS avg_parameter_score
         FROM teams t
         JOIN scores s ON t.team_id = s.team_id
@@ -67,27 +68,46 @@ export const getParameterWiseScores = async (req: Request, res: Response) => {
   }
 };
 
-// SELECT
-// t.team_id,
-// t.team_name,
-// p.parameter_name,
-// AVG(s.score_value) AS avg_parameter_score
-// FROM teams t
-// JOIN scores s ON t.team_id = s.team_id
-// JOIN parameters p ON s.parameter_id = p.parameter_id
-// GROUP BY t.team_id, t.team_name, p.parameter_name
-// HAVING AVG(s.score_value) = (
-// SELECT MAX(avg_parameter_score)
-// FROM (
-// SELECT
-// t.team_id,
-// p.parameter_name,
-// AVG(s.score_value) AS avg_parameter_score
-// FROM teams t
-// JOIN scores s ON t.team_id = s.team_id
-// JOIN parameters p ON s.parameter_id = p.parameter_id
-// WHERE t.comptetition_id = 4
-// GROUP BY t.team_id, p.parameter_name
-// ) AS sub
-// WHERE sub.parameter_name = p.parameter_name
-// );
+export const getparameterResult = async (req: Request, res: Response) => {
+  try {
+    const { competition_id } = req.headers;
+
+    const getParameterResultQuery = `
+        SELECT
+        t.team_id,
+        t.team_name,
+        p.parameter_name,
+        AVG(s.score_value) AS avg_parameter_score
+        FROM teams t
+        JOIN scores s ON t.team_id = s.team_id
+        JOIN parameters p ON s.parameter_id = p.parameter_id
+        WHERE t.competition_id = $1
+        GROUP BY t.team_id, t.team_name, p.parameter_name
+        HAVING AVG(s.score_value) = (
+            SELECT MAX(avg_parameter_score)
+            FROM (
+                SELECT
+                    t.team_id,
+                    p.parameter_name,
+                    AVG(s.score_value) AS avg_parameter_score
+                FROM teams t
+                JOIN scores s ON t.team_id = s.team_id
+                JOIN parameters p ON s.parameter_id = p.parameter_id
+                GROUP BY t.team_id, p.parameter_name
+            ) AS sub
+        WHERE sub.parameter_name = p.parameter_name
+  );
+ `;
+
+    let retrieveResults = await queryDatabase(getParameterResultQuery, [
+      competition_id,
+    ]);
+
+    res.status(200).json({ result: retrieveResults, success: true });
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error in fetching in parameter based results",
+      success: false,
+    });
+  }
+};
--- routes/result.ts
+++ routes/result.ts
@@ -6,9 +6,11 @@ import fetchCompetition from "../middleware/fetchCompetition";
 import {
   getOverallResult,
   getParameterWiseScores,
+  getparameterResult,
 } from "../controllers/result";
 
 router.get("/final", fetchCompetition, getOverallResult);
 router.get("/allteams", fetchCompetition, getParameterWiseScores);
+router.get("/parameterresult", fetchCompetition, getparameterResult);
 
 export default router;
